f484d13eb1ca6331b51aaeea45452364cb90e372
Muhammad Anas Raza <memanasraza@gmail.com>
2024-02-26 21:50:53 -0500

    use ops for converting to numpy :)

--- k3_addons/losses/kappa_loss_test.py
+++ k3_addons/losses/kappa_loss_test.py
@@ -7,4 +7,4 @@ def test_kappa_loss():
     y_pred = ops.convert_to_tensor([[0.1, 0.2, 0.6, 0.1], [0.1, 0.5, 0.3, 0.1],[0.8, 0.05, 0.05, 0.1], [0.01, 0.09, 0.1, 0.8]])
     kappa_loss = WeightedKappaLoss(num_classes=4)
     loss = kappa_loss(y_true, y_pred)
-    np.allclose(loss.numpy(), -1.1611925)
+    np.allclose(ops.convert_to_numpy(loss), -1.1611925)
